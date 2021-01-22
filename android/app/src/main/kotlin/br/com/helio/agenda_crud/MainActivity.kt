032a9a56327c555663f778f4a591989a65e3258f
heliokamakawa <heliokamakawa@gmail.com>
2021-01-21 22:35:07 -0300

    curso_flutter_aula_01

---
+++ android/app/src/main/kotlin/br/com/helio/agenda_crud/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.helio.agenda_crud
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+import 'app/my_app.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
