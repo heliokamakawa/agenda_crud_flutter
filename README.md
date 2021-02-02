11fd0a1d1dcbb81da8cbe22a8dc11375478705b6
heliokamakawa <heliokamakawa@gmail.com>
2021-02-02 17:54:47 -0300

    Lista de contatos com list<map>

--- README.md
+++ README.md
@@ -3,8 +3,13 @@ Curso Flutter - CRUD completo com SQLITE/mobx adotando entidade/serviço/reposit
 
 <h3>Criando, configurando e organizando o projeto</h3> 
 <p>Acesso o projeto <a href='https://github.com/heliokamakawa/agenda_crud/tree/1af4a592dedb2c5383e167941fbe9ce0421aa396'>aqui</a> </p> 
-Link videoaula: https://www.youtube.com/watch?v=9zGYQBooqnM
-
+<p>Link videoaula: https://www.youtube.com/watch?v=9zGYQBooqnM</p>
+<hr/>
 <h3>Definindo Rotas Nomeadas</h3> 
 <p>Acesse o projeto inicial <a href='https://github.com/heliokamakawa/agenda_crud/tree/1af4a592dedb2c5383e167941fbe9ce0421aa396'>aqui</a> </p> 
-Link videoaula: https://www.youtube.com/watch?v=JoNhT_kiuaQ
+<p>Acesse a projeto final de aula <a href='https://github.com/heliokamakawa/agenda_crud/tree/b8b7cf0837f0b3d8b131714aa8c8c21c6cff7d80'>aqui</a> </p> 
+<p>Link videoaula: https://www.youtube.com/watch?v=JoNhT_kiuaQ</p>
+<hr/>
+<h3>Definindo a lista de contatos com ListView e ListTile</h3> 
+<p>Acesse o projeto inicial <a href='https://github.com/heliokamakawa/agenda_crud/tree/b8b7cf0837f0b3d8b131714aa8c8c21c6cff7d80'>aqui</a> </p> 
+<p>Link videoaula: </p>
--- lib/app/view/contact_list.dart
+++ lib/app/view/contact_list.dart
@@ -2,6 +2,12 @@ import 'package:agenda_crud/app/my_app.dart';
 import 'package:flutter/material.dart';
 
 class ContactList  extends StatelessWidget {
+  final lista = [
+    {'nome': 'Pietro', 'telefone' : '(11) 9 9874-5656', 'avatar' : 'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png'},
+    {'nome': 'Maitê', 'telefone' : '(11) 9 9632-8578', 'avatar' : 'https://cdn.pixabay.com/photo/2021/01/17/09/11/woman-5924366_960_720.jpg'},
+    {'nome': 'Hortência', 'telefone' : '(11) 9 9562-3356', 'avatar' : 'https://cdn.pixabay.com/photo/2021/01/24/09/28/girl-5944691_960_720.jpg'},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +22,27 @@ class ContactList  extends StatelessWidget {
              )
         ],
       ),
-      body: ListView(),
-    );
+      body: ListView.builder(
+        itemCount: lista.length,
+        itemBuilder: (context, i){
+          var contato = lista[i];
+          var avatar = CircleAvatar( backgroundImage: NetworkImage(contato['avatar']),);
+          return ListTile(
+            leading: avatar,
+            title: Text(contato['nome']),
+            subtitle:  Text(contato['telefone']),
+            trailing: Container(
+              width: 100,
+              child: Row(
+                children: [
+                  IconButton(icon: Icon(Icons.edit), onPressed: null),
+                  IconButton(icon: Icon(Icons.delete), onPressed: null),
+                ],
+                ),
+              ) ,
+          );
+        },
+      )
+      );
   }
 }
