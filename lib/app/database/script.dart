2a137a0a1aed9bf8dad5793a5d30c291a4f0411f
heliokamakawa <heliokamakawa@gmail.com>
2021-02-09 15:05:52 -0300

    lista com sqflite

--- lib/app/database/script.dart
+++ lib/app/database/script.dart
@@ -2,7 +2,7 @@
 
 final createTable = '''
   CREATE TABLE contact(
-    id INT NOT NULL PRIMARY KEY
+    id INT PRIMARY KEY
     ,nome VARCHAR(200) NOT NULL
     ,telefone CHAR(16) NOT NULL
     ,email VARCHAR(150) NOT NULL
@@ -16,11 +16,11 @@ VALUES ('Pietro','(11) 9 9874-5656','pietro@email.com','https://cdn.pixabay.com/
 ''';
 
 final insert2 = '''
-INSERT INTO contact (nome, telefone, email, url_avatar)
+INSERT INTO contact(nome, telefone, email, url_avatar)
 VALUES ('Maitê','(11) 9 9632-8578', 'maite@email.com','https://cdn.pixabay.com/photo/2021/01/17/09/11/woman-5924366_960_720.jpg')
 ''';
 
-final inser3 = '''
+final insert3 = '''
 INSERT INTO contact (nome, telefone, email, url_avatar)
 VALUES ('Hortência','(11) 9 9562-3356','hortencia@email.com','https://cdn.pixabay.com/photo/2021/01/24/09/28/girl-5944691_960_720.jpg')
 ''';
--- lib/app/view/contact_list.dart
+++ lib/app/view/contact_list.dart
@@ -1,16 +1,35 @@
-import 'package:agenda_crud/app/my_app.dart';
+import 'package:agenda_crud/app/database/script.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+import '../my_app.dart';
 
 class ContactList  extends StatelessWidget {
-  final lista = [
-    {'nome': 'Pietro', 'telefone' : '(11) 9 9874-5656', 'avatar' : 'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png'},
-    {'nome': 'Maitê', 'telefone' : '(11) 9 9632-8578', 'avatar' : 'https://cdn.pixabay.com/photo/2021/01/17/09/11/woman-5924366_960_720.jpg'},
-    {'nome': 'Hortência', 'telefone' : '(11) 9 9562-3356', 'avatar' : 'https://cdn.pixabay.com/photo/2021/01/24/09/28/girl-5944691_960_720.jpg'},
-  ];
+
+  Future<List<Map<String,dynamic>>> _buscar() async{
+    String path = join( await getDatabasesPath(), 'banco');
+    Database db = await openDatabase(
+      path, 
+      version: 1, 
+      onCreate: (db, v){
+        db.execute(createTable);
+        db.execute(insert1);
+        db.execute(insert2);
+        db.execute(insert3);
+      }
+      );
+      return db.query('contact');
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return FutureBuilder(
+      future: _buscar(),
+      builder: (context, futuro){
+        if(futuro.hasData){
+          var lista = futuro.data;
+          return Scaffold(
       appBar: AppBar(
         title: Text('Lista de Contatos'),
         actions: [
@@ -26,7 +45,7 @@ class ContactList  extends StatelessWidget {
         itemCount: lista.length,
         itemBuilder: (context, i){
           var contato = lista[i];
-          var avatar = CircleAvatar( backgroundImage: NetworkImage(contato['avatar']),);
+          var avatar = CircleAvatar( backgroundImage: NetworkImage(contato['url_avatar']),);
           return ListTile(
             leading: avatar,
             title: Text(contato['nome']),
@@ -44,5 +63,12 @@ class ContactList  extends StatelessWidget {
         },
       )
       );
+
+        }else{
+          return Scaffold();
+        }
+      }
+      );
+    
   }
 }
