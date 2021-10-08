79622a35720dfbbaf91392b3a6712279c1d399c9
heliokamakawa <heliokamakawa@gmail.com>
2021-10-08 12:42:37 -0300

    http config initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:agenda_crud/app/injection.dart';
 import 'package:flutter/material.dart';
 import 'app/my_app.dart';
+import 'package:http/http.dart' as http;
 
 void main() async {
+  var resposta = await http.get('http://192.168.1.8:8080/contato');
+  print(resposta.body);
   setupInjection();
   runApp(MyApp());
 }
